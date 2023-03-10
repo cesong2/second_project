5ace3c4cca0fc0a27de2bd53fea21a9b305e97f3
cesong <121409481+cesong2@users.noreply.github.com>
2023-03-10 14:53:53 +0900

    Add files via upload

---
+++ 연습.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE ontime;
+
+SHOW DATABASE;
+
+USE ontime;
+
+create table ontime (
+  Year int,
+  Month int,
+  DayofMonth int,
+  DayOfWeek int,
+  DepTime  int,
+  CRSDepTime int,
+  ArrTime int,
+  CRSArrTime int,
+  UniqueCarrier varchar(5),
+  FlightNum int,
+  TailNum varchar(8),
+  ActualElapsedTime int,
+  CRSElapsedTime int,
+  AirTime int,
+  ArrDelay int,
+  DepDelay int,
+  Origin varchar(3),
+  Dest varchar(3),
+  Distance int,
+  TaxiIn int,
+  TaxiOut int,
+  Cancelled int,
+  CancellationCode varchar(1),
+  Diverted varchar(1),
+  CarrierDelay int,
+  WeatherDelay int,
+  NASDelay int,
+  SecurityDelay int,
+  LateAircraftDelay int
+);
+
+SHOW TABLES;
+
+SELECT uniquecarrier, AVG(depdelay)
+FROM ontime
+GROUP BY uniquecarrier;
